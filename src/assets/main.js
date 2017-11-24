984e3fd1f4a7fef758e2ce85867f6a4473563e2f
Nico <nicolasmm89@gmail.com>
2017-11-24 16:45:59 +0100

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url:'https://www.codeschool.com/users/NicoMed.json',
+    dataType: 'jsonp',
+    success: function(response){
+      console.log(response);
+      var completedCourses= response.courses.completed;
+      for (var i = 0; i<completedCourses.length; i++){
+        console.log(completedCourses[i]);
+        var div = $('<div></div>').appendTo('#badges').addClass('.course');
+          $('<h3>'+completedCourses[i].title+'</h3>').appendTo(div);
+          $('<img></img>').appendTo(div).attr('src',completedCourses[i].badge);
+          $('<a></a>').appendTo(div).attr({
+                                           'href':completedCourses[i].url,
+                                           'target': '_blank'
+                                         }).addClass('btn btn-primary').text('See Course');
+      }
+
+    }
+  });
 
 });
